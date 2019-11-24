ec5f54aea226a022489f4e4b9ef8edb26cbc0dc6
WebGL3D <OgreLandBob@gmail.com>
2019-11-24 11:15:43 -0800

    let's see what we can use

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,3 @@
-echo 2.0.$GITHUB_ACTION
-echo $GITHUB_ACTION
+printenv
 dotnet build ./../TixFactory.CookieJar/TixFactory.CookieJar.sln -p:Version=2.0.$GITHUB_ACTION --configuration Release
 dotnet build ./../TixFactory.Http/TixFactory.Http.sln -p:Version=2.0.$GITHUB_ACTION --configuration Release
