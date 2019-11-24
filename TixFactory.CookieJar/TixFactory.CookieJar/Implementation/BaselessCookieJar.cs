df9e833a5fa3d91167912a5df0b19d909ba8a18e
WebGL3D <OgreLandBob@gmail.com>
2019-11-24 10:02:42 -0800

    Add TixFactory.CookieJar

---
+++ TixFactory.CookieJar/TixFactory.CookieJar/Implementation/BaselessCookieJar.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace TixFactory.CookieJar
+{
+	/// <summary>
+	/// An <see cref="ICookieJar"/> with no backing.
+	/// </summary>
+	/// <inheritdoc cref="ICookieJar"/>
+	public class BaselessCookieJar : ICookieJar
+	{
+		/// <inheritdoc cref="ICookieJar.CookieContainer"/>
+		public CookieContainer CookieContainer { get; }
+
+		/// <summary>
+		/// Initializes a new <see cref="BaselessCookieJar"/>.
+		/// </summary>
+		/// <param name="cookieContainer">The <see cref="CookieContainer"/> (if <c>null</c> initializes a new one.)</param>
+		public BaselessCookieJar(CookieContainer cookieContainer = null)
+		{
+			CookieContainer = cookieContainer ?? new CookieContainer();
+		}
+
+		/// <inheritdoc cref="ICookieJar.Save"/>
+		public void Save()
+		{
+		}
+	}
+}
---
+++ TixFactory.CookieJar/TixFactory.CookieJar/Interfaces/ICookieJar.cs
@@ -0,0 +1,18 @@
+﻿namespace TixFactory.CookieJar
+{
+	/// <summary>
+	/// A wrapper for <see cref="CookieContainer"/> that allows saving the cookies inside.
+	/// </summary>
+	public interface ICookieJar
+	{
+		/// <summary>
+		/// The <see cref="CookieContainer"/>.
+		/// </summary>
+		System.Net.CookieContainer CookieContainer { get; }
+
+		/// <summary>
+		/// Saves the cookies to disk with File Name used to construct the <see cref="ICookieJar"/>.
+		/// </summary>
+		void Save();
+	}
+}
