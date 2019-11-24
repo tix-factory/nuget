0a3b7a6bc35ec1efa8608e5b54d309ad38447aff
WebGL3D <OgreLandBob@gmail.com>
2019-11-24 10:58:42 -0800

    Try to figure out this build thing

---
+++ build/build.sh
@@ -0,0 +1,4 @@
+echo 2.0.$GITHUB_ACTION
+echo $GITHUB_ACTION
+dotnet build ./../TixFactory.CookieJar/TixFactory.CookieJar.sln -p:Version=2.0.$GITHUB_ACTION --configuration Release
+dotnet build ./../TixFactory.Http/TixFactory.Http.sln -p:Version=2.0.$GITHUB_ACTION --configuration Release
