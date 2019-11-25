5e5555a16c59398bb8bbbd57c5a87fa1edafd0db
WebGL3D <OgreLandBob@gmail.com>
2019-11-24 16:47:51 -0800

    Add nuget.config file

---
+++ build/TixFactory.NuGetCredentialsConfig/TixFactory.NuGetCredentialsConfig/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace TixFactory.NuGetCredentialsConfig
+{
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var configFile = args[0];
+			var githubToken = args[1];
+
+			var fileName = Path.GetFileName(configFile);
+			if(!File.Exists(configFile) || fileName != "NuGet.config")
+			{
+				Console.WriteLine("Invalid config file.");
+				Environment.Exit(1);
+				return;
+			}
+
+			var fileText = File.ReadAllText(configFile);
+			fileText = fileText.Replace("GITHUB_TOKEN", githubToken);
+
+			File.WriteAllText(configFile, fileText);
+		}
+	}
+}
