2811b4c8e53bb5488d441a69667ec9574c5c1870
WebGL3D <OgreLandBob@gmail.com>
2019-11-28 14:03:07 -0800

    Add ISetting to TixFactory.Configuration

---
+++ TixFactory.Configuration/TixFactory.Configuration/Implementation/Setting.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace TixFactory.Configuration
+{
+	/// <inheritdoc cref="ISetting{T}"/>
+	public class Setting<T> : ISetting<T>
+	{
+		private T _CurrentValue;
+
+		/// <inheritdoc cref="ISetting{T}.Changed"/>
+		public event Action<T, T> Changed;
+
+		/// <inheritdoc cref="ISetting{T}.Value"/>
+		public T Value
+		{
+			get => _CurrentValue;
+			set
+			{
+				var currentValue = _CurrentValue;
+				if (!value.Equals(currentValue))
+				{
+					_CurrentValue = value;
+					Changed?.Invoke(value, currentValue);
+				}
+			}
+		}
+
+		/// <summary>
+		/// Initializes a new <see cref="Setting{T}"/>.
+		/// </summary>
+		public Setting()
+			: this(default(T))
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new <see cref="Setting{T}"/> with a default <see cref="Value"/>.
+		/// </summary>
+		/// <param name="defaultValue">The default <see cref="Value"/>.</param>
+		public Setting(T defaultValue)
+		{
+			_CurrentValue = defaultValue;
+		}
+	}
+}
---
+++ TixFactory.Configuration/TixFactory.Configuration/Interfaces/ISetting.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TixFactory.Configuration
+{
+	/// <summary>
+	/// Represents a setting that can be changed.
+	/// </summary>
+	/// <typeparam name="T">The setting type.</typeparam>
+	public interface ISetting<T>
+	{
+		/// <summary>
+		/// An event triggered when the <see cref="Value"/> changes.
+		/// </summary>
+		/// <remarks>
+		/// The action first parameter is the new value, the second parameter is the old value.
+		/// </remarks>
+		event Action<T, T> Changed;
+
+		/// <summary>
+		/// The setting value.
+		/// </summary>
+		T Value { get; set; }
+	}
+}
