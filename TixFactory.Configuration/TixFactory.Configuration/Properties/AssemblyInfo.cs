60456f992f2553f037c5f39c187de0c3237dca1d
WebGL3D <OgreLandBob@gmail.com>
2019-11-28 14:07:54 -0800

    Add missing AssemblyInfos

---
+++ TixFactory.Configuration/TixFactory.Configuration/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TixFactory.Configuration.Tests.Unit")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // FakeItEasy
---
+++ TixFactory.Firebase/TixFactory.Firebase/Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("TixFactory.Firebase.Tests.Unit")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // FakeItEasy
