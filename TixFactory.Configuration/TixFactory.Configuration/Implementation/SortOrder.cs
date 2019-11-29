0d848841be773d935f3925d9b99aae01432caa08
WebGL3D <OgreLandBob@gmail.com>
2019-11-29 08:43:41 -0800

    Add SortOrder enum

---
+++ TixFactory.Configuration/TixFactory.Configuration/Implementation/SortOrder.cs
@@ -0,0 +1,21 @@
+﻿namespace TixFactory.Configuration
+{
+	/// <summary>
+	/// A simple sort order enum.
+	/// </summary>
+	/// <remarks>
+	/// Lives in this assembly for lack of a better home.
+	/// </remarks>
+	public enum SortOrder
+	{
+		/// <summary>
+		/// Ascending.
+		/// </summary>
+		Ascending,
+
+		/// <summary>
+		/// Descending.
+		/// </summary>
+		Descending
+	}
+}
