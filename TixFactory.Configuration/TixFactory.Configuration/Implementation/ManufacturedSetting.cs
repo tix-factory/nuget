2451a4d7909ae5c6550b38a6d43a2595fe2ddb30
WebGL3D <OgreLandBob@gmail.com>
2019-11-28 17:09:22 -0800

    Add IManufacturedSetting

---
+++ TixFactory.Configuration/TixFactory.Configuration/Implementation/ManufacturedSetting.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TixFactory.Configuration
+{
+	/// <inheritdoc cref="IManufacturedSetting{T}"/>
+	public class ManufacturedSetting<T> : IManufacturedSetting<T>
+	{
+		private readonly bool _RefreshOnRead;
+		private readonly Func<T> _ValueFactory;
+		private T _CurrentValue;
+
+		/// <inheritdoc cref="ISetting{T}.Changed"/>
+		public event Action<T, T> Changed;
+
+		/// <inheritdoc cref="ISetting{T}.Value"/>
+		public T Value
+		{
+			get
+			{
+				if (_RefreshOnRead)
+				{
+					Refresh();
+				}
+
+				return _CurrentValue;
+			}
+			set => throw new NotSupportedException($"{nameof(ManufacturedSetting<T>)}.{nameof(Value)} cannot be modified.");
+		}
+		
+		/// <summary>
+		/// Initializes a new <see cref="Setting{T}"/> with a default <see cref="Value"/>.
+		/// </summary>
+		public ManufacturedSetting(Func<T> valueFactory, bool refreshOnRead)
+		{
+			_ValueFactory = valueFactory ?? throw new ArgumentNullException(nameof(valueFactory));
+			_RefreshOnRead = refreshOnRead;
+		}
+
+		/// <inheritdoc cref="IManufacturedSetting{T}.Refresh"/>
+		public void Refresh()
+		{
+			var originalValue = _CurrentValue;
+			var newValue = _ValueFactory();
+
+			if (!newValue.Equals(originalValue))
+			{
+				_CurrentValue = newValue;
+				Changed?.Invoke(newValue, originalValue);
+			}
+		}
+	}
+}
---
+++ TixFactory.Configuration/TixFactory.Configuration/Interfaces/IManufacturedSetting.cs
@@ -0,0 +1,14 @@
+﻿namespace TixFactory.Configuration
+{
+	/// <summary>
+	/// A manufactured setting is a setting which has a value that comes from a factory.
+	/// </summary>
+	/// <typeparam name="T">The setting value type.</typeparam>
+	public interface IManufacturedSetting<T> : ISetting<T>
+	{
+		/// <summary>
+		/// Checks the <see cref="ISetting{T}.Value"/> for the setting value from the factory.
+		/// </summary>
+		void Refresh();
+	}
+}
