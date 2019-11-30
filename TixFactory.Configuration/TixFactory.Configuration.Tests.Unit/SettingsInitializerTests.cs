37e3bf31f875e37028a576fb139ac20a840acc4a
WebGL3D <OgreLandBob@gmail.com>
2019-11-30 09:09:14 -0800

    Add TixFactory.Configuration.Tests.Unit

---
+++ TixFactory.Configuration/TixFactory.Configuration.Tests.Unit/SettingsInitializerTests.cs
@@ -0,0 +1,36 @@
+using System.Linq;
+using NUnit.Framework;
+
+namespace TixFactory.Configuration.Tests.Unit
+{
+	[TestFixture]
+	public class SettingsInitializerTests
+	{
+		internal const string DefaultStrValue = "Hello, world!";
+
+		private SettingsInitializer _SettingsInitializer;
+
+		[SetUp]
+		public void Setup()
+		{
+			var baselessSettingValueSource = new BaselessSettingValueSource();
+			_SettingsInitializer = new SettingsInitializer(baselessSettingValueSource);
+		}
+
+		[Test]
+		public void Test()
+		{
+			var settings = _SettingsInitializer.CreateFromInterface<ISettings>();
+			Assert.That(settings.Str, Is.EqualTo(DefaultStrValue));
+
+			var newStrValue = new string(settings.Str.Reverse().ToArray());
+			settings.Str = newStrValue;
+			Assert.That(settings.Str, Is.EqualTo(newStrValue));
+
+			Assert.That(settings.StrWithoutDefaultValue, Is.EqualTo(default(string)));
+
+			settings.StrWithoutDefaultValue = newStrValue;
+			Assert.That(settings.StrWithoutDefaultValue, Is.EqualTo(newStrValue));
+		}
+	}
+}
---
+++ TixFactory.Configuration/TixFactory.Configuration.Tests.Unit/TestItems/ISettings.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace TixFactory.Configuration.Tests.Unit
+{
+	public interface ISettings : Configuration.ISettings
+	{
+		[DefaultValue(SettingsInitializerTests.DefaultStrValue)]
+		string Str { get; set; }
+
+		string StrWithoutDefaultValue { get; set; }
+	}
+}
