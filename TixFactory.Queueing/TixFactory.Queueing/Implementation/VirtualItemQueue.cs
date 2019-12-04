2c4a0dc3ec07f1f51bc27a6b7171ccda32b656b1
WebGL3D <OgreLandBob@gmail.com>
2019-12-03 19:22:13 -0800

    ok so the locking is important to avoid null refs

--- TixFactory.Queueing/TixFactory.Queueing/Implementation/VirtualItemQueue.cs
+++ TixFactory.Queueing/TixFactory.Queueing/Implementation/VirtualItemQueue.cs
@@ -13,6 +13,7 @@ namespace TixFactory.Queueing
 	/// <typeparam name="TItem">The item type.</typeparam>
 	public class VirtualItemQueue<TItem> : IItemQueue<TItem>
 	{
+		private readonly SemaphoreSlim _QueueLock;
 		private readonly IList<QueueItem<TItem>> _Queue;
 		private readonly IManufacturedSetting<long> _QueueSize;
 		private readonly IManufacturedSetting<long> _HeldQueueSize;
@@ -28,6 +29,7 @@ namespace TixFactory.Queueing
 		/// </summary>
 		public VirtualItemQueue()
 		{
+			_QueueLock = new SemaphoreSlim(1, 1);
 			_Queue = new List<QueueItem<TItem>>();
 			_QueueSize = new ManufacturedSetting<long>(() => _Queue.Count, refreshOnRead: true);
 			_HeldQueueSize = new ManufacturedSetting<long>(() => _Queue.Count(IsHeld), refreshOnRead: true);
@@ -36,10 +38,19 @@ namespace TixFactory.Queueing
 		/// <inheritdoc cref="IItemQueue{TItem}.AppendItemToQueue"/>
 		public void AppendItemToQueue(TItem item)
 		{
-			var queueItem = new QueueItem<TItem>(Guid.NewGuid().ToString(), item);
+			_QueueLock.Wait();
 
-			_Queue.Add(queueItem);
-			CheckSizes();
+			try
+			{
+				var queueItem = new QueueItem<TItem>(Guid.NewGuid().ToString(), item);
+
+				_Queue.Add(queueItem);
+				CheckSizes();
+			}
+			finally
+			{
+				_QueueLock.Release();
+			}
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.AppendItemToQueueAsync"/>
@@ -52,23 +63,32 @@ namespace TixFactory.Queueing
 		/// <inheritdoc cref="IItemQueue{TItem}.TryGetNextQueueItem"/>
 		public bool TryGetNextQueueItem(TimeSpan lockExpiration, out QueueItem<TItem> queueItem)
 		{
-			queueItem = _Queue.FirstOrDefault(i => !IsHeld(i));
-			if (queueItem == null)
-			{
-				return false;
-			}
+			_QueueLock.Wait();
 
-			queueItem.HolderId = Guid.NewGuid().ToString();
-			queueItem.LockExpiration = DateTime.UtcNow + lockExpiration;
-			CheckSizes();
-
-			ThreadPool.QueueUserWorkItem(state =>
+			try
 			{
-				Thread.Sleep(lockExpiration);
+				queueItem = _Queue.FirstOrDefault(i => !IsHeld(i));
+				if (queueItem == null)
+				{
+					return false;
+				}
+
+				queueItem.HolderId = Guid.NewGuid().ToString();
+				queueItem.LockExpiration = DateTime.UtcNow + lockExpiration;
 				CheckSizes();
-			});
 
-			return true;
+				ThreadPool.QueueUserWorkItem(state =>
+				{
+					Thread.Sleep(lockExpiration);
+					CheckSizes();
+				});
+
+				return true;
+			}
+			finally
+			{
+				_QueueLock.Release();
+			}
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.TryGetNextQueueItem"/>
@@ -81,14 +101,23 @@ namespace TixFactory.Queueing
 		/// <inheritdoc cref="IItemQueue{TItem}.RemoveQueueItem"/>
 		public void RemoveQueueItem(string id, string holderId)
 		{
-			var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
-			if (item == null)
-			{
-				return;
-			}
+			_QueueLock.Wait();
 
-			_Queue.Remove(item);
-			CheckSizes();
+			try
+			{
+				var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
+				if (item == null)
+				{
+					return;
+				}
+
+				_Queue.Remove(item);
+				CheckSizes();
+			}
+			finally
+			{
+				_QueueLock.Release();
+			}
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.RemoveQueueItemAsync"/>
@@ -101,14 +130,23 @@ namespace TixFactory.Queueing
 		/// <inheritdoc cref="IItemQueue{TItem}.ReleaseQueueItem"/>
 		public void ReleaseQueueItem(string id, string holderId)
 		{
-			var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
-			if (item == null)
-			{
-				return;
-			}
+			_QueueLock.Wait();
 
-			item.LockExpiration = DateTime.MinValue;
-			CheckSizes();
+			try
+			{
+				var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
+				if (item == null)
+				{
+					return;
+				}
+
+				item.LockExpiration = DateTime.MinValue;
+				CheckSizes();
+			}
+			finally
+			{
+				_QueueLock.Release();
+			}
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.ReleaseQueueItemAsync"/>
@@ -121,8 +159,17 @@ namespace TixFactory.Queueing
 		/// <inheritdoc cref="IItemQueue{TItem}.Clear"/>
 		public void Clear()
 		{
-			_Queue.Clear();
-			CheckSizes();
+			_QueueLock.Wait();
+
+			try
+			{
+				_Queue.Clear();
+				CheckSizes();
+			}
+			finally
+			{
+				_QueueLock.Release();
+			}
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.ClearAsync"/>
