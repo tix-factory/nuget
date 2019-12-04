9292321a0c5c6bf294f8a36c3c833b84daba5f10
WebGL3D <OgreLandBob@gmail.com>
2019-12-03 18:45:58 -0800

    asynchronous isn't really a factor in VirtualItemQueue I don't think

--- TixFactory.Queueing/TixFactory.Queueing/Implementation/VirtualItemQueue.cs
+++ TixFactory.Queueing/TixFactory.Queueing/Implementation/VirtualItemQueue.cs
@@ -13,7 +13,6 @@ namespace TixFactory.Queueing
 	/// <typeparam name="TItem">The item type.</typeparam>
 	public class VirtualItemQueue<TItem> : IItemQueue<TItem>
 	{
-		private readonly SemaphoreSlim _QueueLock;
 		private readonly IList<QueueItem<TItem>> _Queue;
 		private readonly IManufacturedSetting<long> _QueueSize;
 		private readonly IManufacturedSetting<long> _HeldQueueSize;
@@ -29,7 +28,6 @@ namespace TixFactory.Queueing
 		/// </summary>
 		public VirtualItemQueue()
 		{
-			_QueueLock = new SemaphoreSlim(1, 1);
 			_Queue = new List<QueueItem<TItem>>();
 			_QueueSize = new ManufacturedSetting<long>(() => _Queue.Count, refreshOnRead: true);
 			_HeldQueueSize = new ManufacturedSetting<long>(() => _Queue.Count(IsHeld), refreshOnRead: true);
@@ -38,219 +36,100 @@ namespace TixFactory.Queueing
 		/// <inheritdoc cref="IItemQueue{TItem}.AppendItemToQueue"/>
 		public void AppendItemToQueue(TItem item)
 		{
-			_QueueLock.Wait();
+			var queueItem = new QueueItem<TItem>(Guid.NewGuid().ToString(), item);
 
-			try
-			{
-				var queueItem = new QueueItem<TItem>(Guid.NewGuid().ToString(), item);
-
-				_Queue.Add(queueItem);
-				CheckSizes();
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			_Queue.Add(queueItem);
+			CheckSizes();
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.AppendItemToQueueAsync"/>
-		public async Task AppendItemToQueueAsync(TItem item, CancellationToken cancellationToken)
+		public Task AppendItemToQueueAsync(TItem item, CancellationToken cancellationToken)
 		{
-			await _QueueLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-
-			try
-			{
-				var queueItem = new QueueItem<TItem>(Guid.NewGuid().ToString(), item);
-
-				_Queue.Add(queueItem);
-				await CheckSizesAsync(cancellationToken).ConfigureAwait(false);
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			AppendItemToQueue(item);
+			return Task.CompletedTask;
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.TryGetNextQueueItem"/>
 		public bool TryGetNextQueueItem(TimeSpan lockExpiration, out QueueItem<TItem> queueItem)
 		{
-			_QueueLock.Wait();
-
-			try
+			queueItem = _Queue.FirstOrDefault(i => !IsHeld(i));
+			if (queueItem == null)
 			{
-				queueItem = _Queue.FirstOrDefault(i => !IsHeld(i));
-				if (queueItem == null)
-				{
-					return false;
-				}
+				return false;
+			}
 
-				queueItem.HolderId = Guid.NewGuid().ToString();
-				queueItem.LockExpiration = DateTime.UtcNow + lockExpiration;
+			queueItem.HolderId = Guid.NewGuid().ToString();
+			queueItem.LockExpiration = DateTime.UtcNow + lockExpiration;
+			CheckSizes();
+
+			ThreadPool.QueueUserWorkItem(state =>
+			{
+				Thread.Sleep(lockExpiration);
 				CheckSizes();
+			});
 
-				ThreadPool.QueueUserWorkItem(state =>
-				{
-					Thread.Sleep(lockExpiration);
-					CheckSizes();
-				});
-
-				return true;
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			return true;
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.TryGetNextQueueItem"/>
-		public async Task<(bool, QueueItem<TItem>)> TryGetNextQueueItemAsync(TimeSpan lockExpiration, CancellationToken cancellationToken)
+		public Task<(bool, QueueItem<TItem>)> TryGetNextQueueItemAsync(TimeSpan lockExpiration, CancellationToken cancellationToken)
 		{
-			await _QueueLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-
-			try
-			{
-				var queueItem = _Queue.FirstOrDefault(i => !IsHeld(i));
-				if (queueItem == null)
-				{
-					return (false, null);
-				}
-
-				queueItem.HolderId = Guid.NewGuid().ToString();
-				queueItem.LockExpiration = DateTime.UtcNow + lockExpiration;
-
-				ThreadPool.QueueUserWorkItem(async state =>
-				{
-					await CheckSizesAsync(cancellationToken).ConfigureAwait(false);
-					await Task.Delay(lockExpiration, cancellationToken).ConfigureAwait(false);
-					await CheckSizesAsync(cancellationToken).ConfigureAwait(false);
-				});
-
-				return (true, queueItem);
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			var hasNextQueueItem = TryGetNextQueueItem(lockExpiration, out var queueItem);
+			return Task.FromResult((hasNextQueueItem, queueItem));
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.RemoveQueueItem"/>
 		public void RemoveQueueItem(string id, string holderId)
 		{
-			_QueueLock.Wait();
-
-			try
+			var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
+			if (item == null)
 			{
-				var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
-				if (item == null)
-				{
-					return;
-				}
+				return;
+			}
 
-				_Queue.Remove(item);
-				CheckSizes();
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			_Queue.Remove(item);
+			CheckSizes();
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.RemoveQueueItemAsync"/>
-		public async Task RemoveQueueItemAsync(string id, string holderId, CancellationToken cancellationToken)
+		public Task RemoveQueueItemAsync(string id, string holderId, CancellationToken cancellationToken)
 		{
-			await _QueueLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-
-			try
-			{
-				var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
-				if (item == null)
-				{
-					return;
-				}
-
-				_Queue.Remove(item);
-				await CheckSizesAsync(cancellationToken).ConfigureAwait(false);
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			RemoveQueueItem(id, holderId);
+			return Task.CompletedTask;
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.ReleaseQueueItem"/>
 		public void ReleaseQueueItem(string id, string holderId)
 		{
-			_QueueLock.Wait();
-
-			try
+			var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
+			if (item == null)
 			{
-				var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
-				if (item == null)
-				{
-					return;
-				}
+				return;
+			}
 
-				item.LockExpiration = DateTime.MinValue;
-				CheckSizes();
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			item.LockExpiration = DateTime.MinValue;
+			CheckSizes();
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.ReleaseQueueItemAsync"/>
-		public async Task ReleaseQueueItemAsync(string id, string holderId, CancellationToken cancellationToken)
+		public Task ReleaseQueueItemAsync(string id, string holderId, CancellationToken cancellationToken)
 		{
-			await _QueueLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-
-			try
-			{
-				var item = _Queue.FirstOrDefault(i => i.Id == id && i.HolderId == holderId);
-				if (item == null)
-				{
-					return;
-				}
-
-				item.LockExpiration = DateTime.MinValue;
-				await CheckSizesAsync(cancellationToken).ConfigureAwait(false);
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			ReleaseQueueItem(id, holderId);
+			return Task.CompletedTask;
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.Clear"/>
 		public void Clear()
 		{
-			_QueueLock.Wait();
-
-			try
-			{
-				_Queue.Clear();
-				CheckSizes();
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			_Queue.Clear();
+			CheckSizes();
 		}
 
 		/// <inheritdoc cref="IItemQueue{TItem}.ClearAsync"/>
-		public async Task ClearAsync(CancellationToken cancellationToken)
+		public Task ClearAsync(CancellationToken cancellationToken)
 		{
-			await _QueueLock.WaitAsync(cancellationToken).ConfigureAwait(false);
-
-			try
-			{
-				_Queue.Clear();
-				await CheckSizesAsync(cancellationToken).ConfigureAwait(false);
-			}
-			finally
-			{
-				_QueueLock.Release();
-			}
+			Clear();
+			return Task.CompletedTask;
 		}
 
 		private bool IsHeld(QueueItem<TItem> queueItem)
@@ -262,11 +141,6 @@ namespace TixFactory.Queueing
 		{
 			_QueueSize.Refresh();
 			_HeldQueueSize.Refresh();
-		}
-
-		private Task CheckSizesAsync(CancellationToken cancellationToken)
-		{
-			return Task.Run(() => CheckSizes(), cancellationToken);
 		}
 	}
 }
