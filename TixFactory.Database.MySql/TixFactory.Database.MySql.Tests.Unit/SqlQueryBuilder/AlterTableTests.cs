821949db07cc55a5663113d78581c4e0dee11061
WebGL3D <OgreLandBob@gmail.com>
2019-12-25 16:39:09 -0800

    Add BuildDropColumnQuery test

---
+++ TixFactory.Database.MySql/TixFactory.Database.MySql.Tests.Unit/SqlQueryBuilder/AlterTableTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace TixFactory.Database.MySql.Tests.Unit
+{
+	public partial class SqlQueryBuilderTests
+	{
+		[TestCase(_ColumnName, ExpectedResult = "ALTER TABLE `" + _DatabaseName + "`.`" + _TableName + "`\n\tDROP COLUMN `" + _ColumnName + "`;")]
+		public string BuildDropColumnQuery_ValidColumnName_ReturnsDropColumnQuery(string columnName)
+		{
+			var query = _SqlQueryBuilder.BuildDropColumnQuery(_DatabaseName, _TableName, columnName);
+
+			Assert.That(query.Parameters, Is.Empty, "Expected DROP COLUMN query to not have parameters.");
+			return query.Query;
+		}
+	}
+}
---
+++ TixFactory.Database.MySql/TixFactory.Database.MySql.Tests.Unit/SqlQueryBuilder/SqlQueryBuilderTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using NUnit.Framework;
+
+namespace TixFactory.Database.MySql.Tests.Unit
+{
+	[TestFixture, ExcludeFromCodeCoverage]
+	public partial class SqlQueryBuilderTests
+	{
+		private const string _DatabaseName = "test_database";
+		private const string _TableName = "test_table";
+		private const string _ColumnName = "test_column";
+
+		private IDatabaseTypeParser _DatabaseTypeParser;
+		private SqlQueryBuilder _SqlQueryBuilder;
+
+		[SetUp]
+		public void SetUp()
+		{
+			_DatabaseTypeParser = new DatabaseTypeParser();
+			_SqlQueryBuilder = new SqlQueryBuilder(_DatabaseTypeParser);
+		}
+	}
+}
