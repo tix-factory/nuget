2e076c9996bca2722d7052c9f2dd919924a68904
WebGL3D <OgreLandBob@gmail.com>
2019-12-26 11:06:42 -0800

    Add BuildCreateTableQuery test

---
+++ TixFactory.Database.MySql/TixFactory.Database.MySql.Tests.Unit/Queries/CreateTableQuery.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE `test_database`.`test_table` (
+	`ID` BIGINT AUTO_INCREMENT PRIMARY KEY,
+	`Name` VARBINARY(50),
+	`Description` TEXT,
+	`Value` INTEGER NULL,
+	`Created` DATETIME,
+	`Updated` DATETIME
+);
---
+++ TixFactory.Database.MySql/TixFactory.Database.MySql.Tests.Unit/SqlQueryBuilder/BuildCreateTableQueryTests.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace TixFactory.Database.MySql.Tests.Unit
+{
+	public partial class SqlQueryBuilderTests
+	{
+		private static IEnumerable<TestCaseData> BuildCreateTableQueryTestCases
+		{
+			get
+			{
+				yield return new TestCaseData()
+					.SetName("{m}_ValidTableRowClass_ReturnsCreateTableQuery")
+					.Returns(GetQuery("CreateTableQuery"));
+			}
+		}
+
+		[TestCaseSource(nameof(BuildCreateTableQueryTestCases))]
+		public string BuildCreateTableQuery()
+		{
+			var query = _SqlQueryBuilder.BuildCreateTableQuery<TestTable>();
+
+			Assert.That(query.Parameters, Is.Empty);
+			return query.Query;
+		}
+	}
+}
