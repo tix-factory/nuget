d8eee78c4300a724e9b3434ceb41f675058fed20
Bob Rose <WebGL3D@users.noreply.github.com>
2020-10-03 11:45:50 -0700

    Support netcoreapp3.1 for HTTP applications (#3)

---
+++ TixFactory.Http/TixFactory.Http.Service/Models/ApplicationMetadataResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace TixFactory.Http.Service
+{
+	/// <summary>
+	/// Metadata about the running application.
+	/// </summary>
+	[DataContract]
+	public class ApplicationMetadataResponse
+	{
+		/// <summary>
+		/// The name of the running application.
+		/// </summary>
+		[DataMember(Name = "name")]
+		public string Name { get; set; }
+	}
+}
