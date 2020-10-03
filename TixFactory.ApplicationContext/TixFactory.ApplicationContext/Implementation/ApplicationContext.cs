1e83489249977bfa648078b2ef16f5713872859b
WebGL3D <OgreLandBob@gmail.com>
2020-10-03 11:37:57 -0700

    Add TixFactory.ApplicationContext

---
+++ TixFactory.ApplicationContext/TixFactory.ApplicationContext/Implementation/ApplicationContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+
+namespace TixFactory.ApplicationContext
+{
+	/// <inheritdoc cref="IApplicationContext"/>
+	public class ApplicationContext : IApplicationContext
+	{
+		/// <inheritdoc cref="IApplicationContext.Name"/>
+		public string Name
+		{
+			get
+			{
+				var path = Assembly?.Location;
+				if (string.IsNullOrWhiteSpace(path))
+				{
+					return null;
+				}
+
+				return Path.GetFileNameWithoutExtension(path);
+			}
+		}
+
+		/// <inheritdoc cref="IApplicationContext.Assembly"/>
+		public Assembly Assembly { get; private set; }
+
+		/// <summary>
+		/// The singleton <see cref="ApplicationContext"/>.
+		/// </summary>
+		public static readonly ApplicationContext Singleton = new ApplicationContext();
+
+		private ApplicationContext()
+		{
+			Assembly = Assembly.GetEntryAssembly();
+		}
+
+		/// <summary>
+		/// Sets the entry class <see cref="Type"/> to base the <see cref="ApplicationContext"/> on.
+		/// </summary>
+		/// <param name="entryClass">The entry class <see cref="Type"/>.</param>
+		/// <exception cref="ArgumentNullException">
+		/// - <paramref name="entryClass"/>
+		/// </exception>
+		/// <exception cref="ArgumentException">
+		/// - <paramref name="entryClass"/>.<see cref="Type.IsClass"/> is <c>false</c>.
+		/// </exception>
+		public static void SetEntryClass(Type entryClass)
+		{
+			if (entryClass == null)
+			{
+				throw new ArgumentNullException(nameof(entryClass));
+			}
+
+			if (!entryClass.IsClass)
+			{
+				throw new ArgumentException($"{nameof(entryClass)} expected to be class type.", nameof(entryClass));
+			}
+
+			Singleton.Assembly = entryClass.Assembly;
+		}
+	}
+}
---
+++ TixFactory.ApplicationContext/TixFactory.ApplicationContext/Interfaces/IApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Reflection;
+
+namespace TixFactory.ApplicationContext
+{
+	/// <summary>
+	/// Context about the running application.
+	/// </summary>
+	public interface IApplicationContext
+	{
+		/// <summary>
+		/// The application assembly name.
+		/// </summary>
+		string Name { get; }
+
+		/// <summary>
+		/// The running application <see cref="Assembly"/>.
+		/// </summary>
+		Assembly Assembly { get; }
+	}
+}
