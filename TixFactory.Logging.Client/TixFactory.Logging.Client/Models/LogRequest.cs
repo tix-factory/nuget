8db4a58a948f2f88504139a41a9b1a188c53b30f
Bob Rose <WebGL3D@users.noreply.github.com>
2020-10-11 18:23:50 -0700

    Add TixFactory.Logging.Client (#5)

---
+++ TixFactory.Logging.Client/TixFactory.Logging.Client/Models/LogRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace TixFactory.Logging.Client
+{
+	[DataContract]
+	internal class LogRequest
+	{
+		[DataMember(Name = "message")]
+		public string Message { get; set; }
+
+		[DataMember(Name = "log")]
+		public LogData Log { get; set; }
+
+		[DataMember(Name = "host")]
+		public HostData Host { get; set; }
+	}
+}
