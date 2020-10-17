3e3cf0f30b5dbd86b27fcdf6fe5a2a12abc1b391
WebGL3D <OgreLandBob@gmail.com>
2020-10-17 10:18:45 -0700

    Add ApplicationAuthorizationsAccessor

---
+++ TixFactory.ApplicationAuthorization/TixFactory.ApplicationAuthorization/Interfaces/IApplicationAuthorizationsAccessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TixFactory.ApplicationAuthorization
+{
+	public interface IApplicationAuthorizationsAccessor
+	{
+		/// <summary>
+		/// Gets the names of authorized operations for a given ApiKey.
+		/// </summary>
+		/// <param name="apiKey">The ApiKey (<see cref="Guid"/>).</param>
+		/// <returns>The names of the operations the ApiKey is authorized to access.</returns>
+		ISet<string> GetAuthorizedOperationNames(Guid apiKey);
+	}
+}
