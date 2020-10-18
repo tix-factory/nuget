015f30610a7cee9f1367fe1b1dbfdae2f40dbe8d
WebGL3D <OgreLandBob@gmail.com>
2020-10-18 08:28:18 -0700

    Add ApiKeyHeaderParser

---
+++ TixFactory.ApplicationAuthorization/TixFactory.ApplicationAuthorization/Interfaces/IApiKeyParser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace TixFactory.ApplicationAuthorization
+{
+	public interface IApiKeyParser
+	{
+		bool TryParseApiKey(HttpRequest request, out Guid apiKey);
+	}
+}
