07c851f50c69fb74d56c65bb505777ffdd56bcf2
Bob Rose <WebGL3D@users.noreply.github.com>
2020-10-25 14:59:33 -0700

    Create TixFactory.Data.MySql (#13)

---
+++ TixFactory.Data.MySql/TixFactory.Data.MySql/Models/CountResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace TixFactory.Data.MySql
+{
+	[DataContract]
+	internal class CountResult
+	{
+		[DataMember(Name = "Count")]
+		public long Count { get; set; }
+	}
+}
---
+++ TixFactory.Data.MySql/TixFactory.Data.MySql/Models/InsertResult.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization;
+
+namespace TixFactory.Data.MySql
+{
+	[DataContract]
+	internal class InsertResult<T>
+	{
+		[DataMember(Name = "ID")]
+		public T Id { get; set; }
+	}
+}
