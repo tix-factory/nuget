364f6e5ae5b85c6e1e292395f29a32e320241537
WebGL3D <OgreLandBob@gmail.com>
2020-11-08 09:44:34 -0800

    Add async support to TixFactory.Data.MySql

--- TixFactory.Data.MySql/TixFactory.Data.MySql/Implementation/DatabaseConnectionWrapper.cs
+++ TixFactory.Data.MySql/TixFactory.Data.MySql/Implementation/DatabaseConnectionWrapper.cs
@@ -1,45 +1,149 @@
 ﻿using System;
 using System.Data;
 using System.Threading;
+using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using TixFactory.Configuration;
+using TixFactory.Logging;
 
 namespace TixFactory.Data.MySql
 {
 	internal class DatabaseConnectionWrapper
 	{
 		private readonly IReadOnlySetting<string> _ConnectionString;
-		private readonly ILazyWithRetry<MySqlConnection> _ConnectionLazy;
+		private readonly ILogger _Logger;
+		private readonly SemaphoreSlim _GetConnectionLock;
+		private MySqlConnection _MySqlConnection;
 
 		public SemaphoreSlim ConnectionLock { get; }
 
-		public MySqlConnection Connection => _ConnectionLazy.Value;
-
-		public DatabaseConnectionWrapper(IReadOnlySetting<string> connectionString)
+		public DatabaseConnectionWrapper(IReadOnlySetting<string> connectionString, ILogger logger)
 		{
 			_ConnectionString = connectionString ?? throw new ArgumentNullException(nameof(connectionString));
-			_ConnectionLazy = new LazyWithRetry<MySqlConnection>(BuildConnection);
+			_Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			_GetConnectionLock = new SemaphoreSlim(1, 1);
 			ConnectionLock = new SemaphoreSlim(1, 1);
+		}
+
+		public MySqlConnection GetConnection()
+		{
+			_GetConnectionLock.Wait();
+
+			try
+			{
+				if (_MySqlConnection != null)
+				{
+					return _MySqlConnection;
+				}
+
+				var connectionString = _ConnectionString.Value;
+				var connection = new MySqlConnection(connectionString);
+				connection.StateChange += ConnectionStateChange;
+				connection.Open();
+
+				return connection;
+			}
+			finally
+			{
+				_GetConnectionLock.Release();
+			}
+		}
+
+		public async Task<MySqlConnection> GetConnectionAsync(CancellationToken cancellationToken)
+		{
+			await _GetConnectionLock.WaitAsync(cancellationToken).ConfigureAwait(false);
+
+			try
+			{
+				if (_MySqlConnection != null)
+				{
+					return _MySqlConnection;
+				}
+
+				var connectionString = _ConnectionString.Value;
+				var connection = new MySqlConnection(connectionString);
+				connection.StateChange += ConnectionStateChange;
+
+				await connection.OpenAsync(cancellationToken).ConfigureAwait(false);
+
+				return connection;
+			}
+			finally
+			{
+				_GetConnectionLock.Release();
+			}
 		}
 
 		public void Close()
 		{
-			if (_ConnectionLazy.IsValueCreated)
+			_GetConnectionLock.Wait();
+
+			try
 			{
-				var connection = _ConnectionLazy.Value;
-				_ConnectionLazy.Refresh();
-				connection.Close();
+				var connection = _MySqlConnection;
+				if (connection != null)
+				{
+					try
+					{
+						connection.Close();
+					}
+					catch (Exception e)
+					{
+						_Logger.Warn($"Unexpected exception while closing connection\n{e}");
+					}
+
+					try
+					{
+						connection.Dispose();
+					}
+					catch
+					{
+						// who cares?
+					}
+				}
+
+				_MySqlConnection = null;
+			}
+			finally
+			{
+				_GetConnectionLock.Release();
 			}
 		}
 
-		private MySqlConnection BuildConnection()
+		public async Task CloseAsync(CancellationToken cancellationToken)
 		{
-			var connectionString = _ConnectionString.Value;
-			var connection = new MySqlConnection(connectionString);
-			connection.StateChange += ConnectionStateChange;
-			connection.Open();
+			await _GetConnectionLock.WaitAsync(cancellationToken).ConfigureAwait(false);
 
-			return connection;
+			try
+			{
+				var connection = _MySqlConnection;
+				if (connection != null)
+				{
+					try
+					{
+						await connection.CloseAsync(cancellationToken).ConfigureAwait(false);
+					}
+					catch (Exception e)
+					{
+						_Logger.Warn($"Unexpected exception while closing connection\n{e}");
+					}
+
+					try
+					{
+						await connection.DisposeAsync().ConfigureAwait(false);
+					}
+					catch
+					{
+						// who cares?
+					}
+				}
+
+				_MySqlConnection = null;
+			}
+			finally
+			{
+				_GetConnectionLock.Release();
+			}
 		}
 
 		private void ConnectionStateChange(object sender, StateChangeEventArgs e)
@@ -48,7 +152,7 @@ namespace TixFactory.Data.MySql
 			{
 				case ConnectionState.Broken:
 				case ConnectionState.Closed:
-					_ConnectionLazy.Refresh();
+					Close();
 					return;
 			}
 		}
