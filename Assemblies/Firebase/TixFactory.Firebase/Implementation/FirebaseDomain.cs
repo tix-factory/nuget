5b4d1a4b2c9a66708540cc6c7df57cf7f3d6aa96
WebGL3D <WebGL3D@users.noreply.github.com>
2022-10-09 19:12:28 -0700

    Deprecate TixFactory.Firebase

--- Assemblies/Firebase/TixFactory.Firebase/Implementation/FirebaseDomain.cs
+++
@@ -1,8 +0,0 @@
-namespace TixFactory.Firebase
-{
-    internal static class FirebaseDomain
-    {
-        public const string Fcm = "fcm.googleapis.com";
-        public const string Iid = "iid.googleapis.com";
-    }
-}
