b25e93d3bf7f1ad4f22f84488bf50453ad237a44
WebGL3D <WebGL3D@users.noreply.github.com>
2022-10-29 09:41:06 -0700

    Remove TixFactory.Discord

--- Assemblies/Discord/TixFactory.Discord/Interfaces/IDiscordClientFactory.cs
+++
@@ -1,9 +0,0 @@
-using Discord.WebSocket;
-
-namespace TixFactory.Discord
-{
-    public interface IDiscordClientFactory
-    {
-        DiscordSocketClient CreateBotClient(string botToken);
-    }
-}
